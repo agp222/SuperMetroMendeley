058239718b45b01b56b3f9cfa878c8b7d69a836f
agp222 <antonio.guzzo@correo.unimet.edu.ve>
2025-11-16 11:42:10 -0400

    Interfaz v1

--- proyecto2/src/proyecto2/proyecto2.java
+++ proyecto2/src/proyecto2/proyecto2.java
@@ -14,7 +14,8 @@ public class proyecto2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
     }
     
 }
