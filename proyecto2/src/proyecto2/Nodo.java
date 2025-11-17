96214315d1a4aee18ccb11148bdef94b4d45518f
agp222 <antonio.guzzo@correo.unimet.edu.ve>
2025-11-17 15:25:39 -0400

    Nodo del hash

---
+++ proyecto2/src/proyecto2/Nodo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author Antonio Guzzo
+ */
+public class Nodo {
+    private String clave;
+    private Articulo valor;
+    private Nodo siguiente;
+
+    Nodo(String clave, Articulo valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public Articulo getValor() {
+        return valor;
+    }
+
+    public void setValor(Articulo valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
+}
