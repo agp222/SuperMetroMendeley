0fd9cfac0e1c0cee7a2ccfb160c2fb0bf58e2953
agp222 <antonio.guzzo@correo.unimet.edu.ve>
2025-11-18 09:00:48 -0400

    JavaDoc Tabla Hash

--- proyecto2/src/proyecto2/Nodo.java
+++ proyecto2/src/proyecto2/Nodo.java
@@ -8,36 +8,77 @@ package proyecto2;
  *
  * @author Antonio Guzzo
  */
+
+
+/**
+ * La clase {@code Nodo} representa un elemento dentro del Hastable. Cada nodo almacena una clave, un Articulo
+ * y una referencia al siguiente nodo en la lista enlazada para manejar colisiones
+ * 
+ */
 public class Nodo {
+    
+    //Atributos
     private String clave;
     private Articulo valor;
     private Nodo siguiente;
 
+    /**
+     * Crea un nuevo nodo con la clave y el valor especificados
+     *
+     * @param clave la clave única asociada al nodo
+     * @param valor el objeto {@link Articulo} que se almacena en el nodo
+     */
     Nodo(String clave, Articulo valor) {
         this.clave = clave;
         this.valor = valor;
     }
 
+    
+    /**
+     * Obtiene la clave del nodo
+     * @return la clave como {@code String}
+     */
     public String getClave() {
         return clave;
     }
 
+    /**
+     * Establece la clave del nodo
+     * @param clave nueva clave
+     */
     public void setClave(String clave) {
         this.clave = clave;
     }
 
+    /**
+     * Obtiene el valor almacenado en el nodo
+     * @return el objeto {@link Articulo} asociado
+     */
     public Articulo getValor() {
         return valor;
     }
 
+    /**
+     * Establece el valor almacenado en el nodo
+     * @param valor nuevo objeto {@link Articulo}
+     */
     public void setValor(Articulo valor) {
         this.valor = valor;
     }
 
+    /**
+     * Obtiene el siguiente nodo en la lista enlazada
+     * @return el siguiente nodo o {@code null} si no existe
+     */
     public Nodo getSiguiente() {
         return siguiente;
     }
 
+    
+    /**
+     * Establece el siguiente nodo en la lista enlazada
+     * @param siguiente referencia al siguiente nodo
+     */
     public void setSiguiente(Nodo siguiente) {
         this.siguiente = siguiente;
     }
